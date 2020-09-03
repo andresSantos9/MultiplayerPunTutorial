cfcaba2cbb914e38bde058fcf9bd64d9b6384e43
Andres Santos Torres <andres@MacBook-Pro-de-Andres.local>
2020-09-03 12:02:11 +0200

    first commit

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Connect();
+    }
+
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Connect()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public void Play()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        //base.OnJoinRandomFailed(returnCode,message);
+
+        Debug.Log("Tried to join a room and failed");
+
+        //most likely no room
+        PhotonNetwork.CreateRoom(null, new RoomOptions {MaxPlayers = 4});
+
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("joined a room!!");
+        if(PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.LoadLevel(1);
+        }
+    }
+}
---
+++ Assets/Simple First Person Controller/Scripts/Health.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Health : MonoBehaviourPunCallbacks, IPunObservable
+{
+    Renderer [] visuals;
+    public int myHealth = 100;
+    public int enemyHealth;
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        //sync health
+        if (stream.IsWriting)
+        {
+            stream.SendNext(myHealth);
+        }
+        else
+        {
+            //we are reading
+            enemyHealth =(int)stream.ReceiveNext();
+
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        myHealth -= damage;
+    }
+    
+    IEnumerator Respawn()
+    {
+        SetRenderers(false);
+        myHealth = 100;
+        GetComponent<CharacterController>().enabled = false;
+        transform.position = new Vector3(0, 10,0);
+        yield return new WaitForSeconds(1);
+        GetComponent<CharacterController>().enabled = true;
+        SetRenderers(true);
+    }
+
+    void SetRenderers(bool state)
+    {
+        foreach(var renderer in visuals)
+        {
+            renderer.enabled = state;
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        visuals = GetComponentsInChildren<Renderer>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (myHealth <=0)
+        {
+            StartCoroutine(Respawn());
+        }
+        
+    }
+}
